b80fc1c9f3eb21f353b268932a18ac3893dcddf8
myreceiptt <nota@endhonesa.com>
2026-05-30 01:52:03 +0700

    chore: run evergreen maintenance

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,17 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      ".next/**",
+      "next-env.d.ts",
+      "node_modules/**",
+      "out/**",
+      "build/**",
+      "coverage/**",
+    ],
+  },
+  ...nextCoreWebVitals,
 ];
 
 export default eslintConfig;
